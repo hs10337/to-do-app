c3fda1f1867fa400937ecb15a1ed3b8a230a70d1
hs10337 <hs10337@gmail.com>
2018-01-16 22:06:04 -0800

    checkpoint 10 dom scripting part 1

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-function onReady() {
+
+function onReady(){
   const addToDoForm = document.getElementById('addToDoForm');
     const newToDoText = document.getElementById('newToDoText');
     const toDoList = document.getElementById('toDoList');
 
-  addToDoForm.addEventListener('submit', () => {
+    addToDoForm.addEventListener('submit', () => {
     event.preventDefault();
-
     // get the text
     let title = newToDoText.value;
 
@@ -19,18 +19,36 @@ function onReady() {
     checkbox.type = "checkbox";
 
     // set the title
-    newLi.textContent = title;
+   newLi.textContent = title;
 
-    // attach the checkbox to the li
-    newLi.appendChild(checkbox);
+   // attach the checkbox to the li
+   newLi.appendChild(checkbox);
 
-    // attach the li to the ul
- toDoList.appendChild(newLi);
+   // attach the li to the ul
+   toDoList.appendChild(newLi);
 
- //empty the input
-newToDoText.value = '';
-  });
-}
+   //empty the input
+     newToDoText.value = '';
+
+  // create button
+  let deleteButton = document.createElement('button');
+
+  // create text
+  let deleteButtonText = document.createTextNode("delete");
+
+  // append text to button
+  deleteButton.appendChild(deleteButtonText);
+
+  // append button to list
+  newLi.appendChild(deleteButton);
+  })
+};
+
 window.onload = function() {
   onReady();
 };
+
+
+document.getElementsByTagName("button").addEventListener("click", () => {
+    document.getElementsByTagName("li").innerHTML = "";
+  });
